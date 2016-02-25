9710e559a51fd4704b7b87a8b7ecae2232945230
dwdineen <dwdineen@umich.edu>
2016-02-24 22:08:26 -0500

    Extended the already in django 'user' model to include extraneous info in a model called 'profile_info'

--- mibiosoft/protocat/models.py
+++ mibiosoft/protocat/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile_Info(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	Profile_Image = models.FileField()
 
 # Create your models here.
 '''
