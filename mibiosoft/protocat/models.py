d117ba1b7e2e8e8f32f730019ddfa63198a590c0
ThomasCOliver <tommy.oliver@live.com>
2016-02-26 22:15:38 -0500

    Added most of Protocol class and basics of Protocol_Category and Reagent classes

--- mibiosoft/protocat/models.py
+++ mibiosoft/protocat/models.py
@@ -1,12 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
+# for Protocol
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime
+
+'''
+This page is to create python classes, which are then converted in to tables for our database
+So things like, User, Protocol, Reagents, etc.
+'''
 
 class Profile_Info(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	Profile_Image = models.FileField()
 
-# Create your models here.
-'''
-This page is to create python classes, which are then converted in to tables for our database
-So things like, User, Protocol, Reagents, etc.
-'''
+class Protocol_Category(models.Model):
+	title = models.TextField()
+	author = models.ForeignKey(User)
+	description = models.TextField()
+	parent_category = models.ForeignKey('self')
+
+class Reagent(models.Model):
+	name = models.TextField()
+
+class Protocol(models.Model):
+	title = models.TextField()
+	author = models.ForeignKey(User)
+	description = models.TextField()
+	# many protocols to one category
+	category = models.ForeignKey(Protocol_Category)
+	is_searchable = models.BooleanField(default=True)
+	upload_date = models.DateField()
+	number_of_ratings = models.IntegerField(default=0)
+	sum_of_ratings = models.IntegerField(default=0)
+	# many branching protocols to one parent protocol
+	last_revision = models.ForeignKey('self')
+	# many different protocols can use one reagent, one protocol can use many reagents
+	reagents = models.ManyToManyField(Reagent)
