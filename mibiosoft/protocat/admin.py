5b9c56c3ca0e5b76efc0c28dd486d8deead274d7
ThomasCOliver <tommy.oliver@live.com>
2016-03-02 22:13:55 -0500

    Added more information/changed names of models/added additional functions so the the admin site looks better and changed the upload_date field in Protocol.

--- mibiosoft/protocat/admin.py
+++ mibiosoft/protocat/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
-from .models import Protocol, Protocol_Category, Reagent, Profile_Info
+from .models import Protocol, Category, Reagent, ProfileInfo
+
+
+
+# Information for a better /admin/ site
+
+class ProtocolAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "category", "author", "last_revision", "upload_date"]
+	class Meta:
+		model = Protocol
+
+class ProfileInfoAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "email", "is_admin", "date_joined"]
+	class Meta:
+		model = ProfileInfo
+
+class ReagentAdmin(admin.ModelAdmin):
+	list_display = ["__str__"]
+	class Meta:
+		model = Reagent
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "parent_category", "author"]
+	class Meta:
+		model = Category
 
 # Register your models here.
-admin.site.register(Protocol)
-admin.site.register(Profile_Info)
-admin.site.register(Reagent)
-admin.site.register(Protocol_Category)
+
+admin.site.register(Protocol, ProtocolAdmin)
+admin.site.register(ProfileInfo, ProfileInfoAdmin)
+admin.site.register(Reagent, ReagentAdmin)
+admin.site.register(Category, CategoryAdmin)
--- mibiosoft/protocat/models.py
+++ mibiosoft/protocat/models.py
@@ -9,27 +9,39 @@ This page is to create python classes, which are then converted in to tables for
 So things like, User, Protocol, Reagents, etc.
 '''
 
-class Profile_Info(models.Model):
+class ProfileInfo(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	Profile_Image = models.FileField()
+	profile_image = models.FileField(null=True, blank=True)
+	def __str__(self):
+		return str(self.user)
+	def email(self):
+		return self.user.email
+	def is_admin(self):
+		return self.user.is_staff
+	def date_joined(self):
+		return self.user.date_joined
 
-class Protocol_Category(models.Model):
+class Category(models.Model):
 	title = models.TextField()
 	author = models.ForeignKey(User)
 	description = models.TextField()
 	parent_category = models.ForeignKey('self', blank=True, null=True)
+	def __str__(self):
+		return self.title;
 
 class Reagent(models.Model):
 	name = models.TextField()
+	def __str__(self):
+		return self.name;
 
 class Protocol(models.Model):
 	title = models.TextField()
 	author = models.ForeignKey(User)
 	description = models.TextField()
 	# many protocols to one category
-	category = models.ForeignKey(Protocol_Category)
+	category = models.ForeignKey(Category)
 	is_searchable = models.BooleanField(default=True)
-	upload_date = models.DateField()
+	upload_date = models.DateTimeField(auto_now_add=True)
 	number_of_ratings = models.IntegerField(default=0)
 	sum_of_ratings = models.IntegerField(default=0)
 	# many branching protocols to one parent protocol
@@ -37,3 +49,5 @@ class Protocol(models.Model):
 	# many different protocols can use one reagent, one protocol can use many reagents
 	reagents = models.ManyToManyField(Reagent)
 	# still need to add steps, which could possible be a separate class instead of just text
+	def __str__(self):
+		return self.title;
