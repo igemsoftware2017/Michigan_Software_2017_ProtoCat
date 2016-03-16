31d84e65e8b6e2ac91e706a4a6f14bc142d99305
ThomasCOliver <tommy.oliver@live.com>
2016-03-16 18:24:41 -0400

    Adding protocat steps to models and admin.

--- mibiosoft/protocat/admin.py
+++ mibiosoft/protocat/admin.py
@@ -1,33 +1,45 @@
 from django.contrib import admin
-from .models import Protocol, Category, Reagent, ProfileInfo
+from .models import ProfileInfo, Reagent, Category, Protocol, ProtocolStep, ReagentForProtocol
 
 
 
 # Information for a better /admin/ site
-
-class ProtocolAdmin(admin.ModelAdmin):
-	list_display = ["__str__", "category", "author", "last_revision", "upload_date"]
-	class Meta:
-		model = Protocol
 
 class ProfileInfoAdmin(admin.ModelAdmin):
 	list_display = ["__str__", "email", "is_admin", "date_joined"]
 	class Meta:
 		model = ProfileInfo
 
-class ReagentAdmin(admin.ModelAdmin):
-	list_display = ["__str__"]
-	class Meta:
-		model = Reagent
-
 class CategoryAdmin(admin.ModelAdmin):
 	list_display = ["__str__", "parent_category", "author"]
 	class Meta:
 		model = Category
 
+class ReagentAdmin(admin.ModelAdmin):
+	list_display = ["__str__"]
+	class Meta:
+		model = Reagent
+
+class ProtocolAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "category", "author", "last_revision", "upload_date"]
+	class Meta:
+		model = Protocol
+
+class ProtocolStepAdmin(admin.ModelAdmin):
+	list_display = ["__str__"]
+	class Meta:
+		model = ProtocolStep
+
+class ReagentForProtocolAdmin(admin.ModelAdmin):
+	list_display = ["__str__"]
+	class Meta:
+		model = ReagentForProtocol
+
 # Register your models here.
 
-admin.site.register(Protocol, ProtocolAdmin)
 admin.site.register(ProfileInfo, ProfileInfoAdmin)
-admin.site.register(Reagent, ReagentAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Reagent, ReagentAdmin)
+admin.site.register(Protocol, ProtocolAdmin)
+admin.site.register(ProtocolStep, ProtocolStepAdmin)
+admin.site.register(ReagentForProtocol, ReagentForProtocolAdmin)
--- mibiosoft/protocat/models.py
+++ mibiosoft/protocat/models.py
@@ -25,29 +25,79 @@ class Category(models.Model):
 	title = models.TextField()
 	author = models.ForeignKey(User)
 	description = models.TextField()
-	parent_category = models.ForeignKey('self', blank=True, null=True)
+	parent_category = models.ForeignKey('self',
+										blank=True,
+										null=True)
 	def __str__(self):
-		return self.title;
+		return self.title
+
 
 class Reagent(models.Model):
 	name = models.TextField()
+	website = models.URLField(null=True, blank=True)
 	def __str__(self):
-		return self.name;
+		return self.name
 
 class Protocol(models.Model):
 	title = models.TextField()
 	author = models.ForeignKey(User)
 	description = models.TextField()
+
+	# Allow the revisionist to describe changes made
+	change_log = models.TextField()
+
 	# many protocols to one category
 	category = models.ForeignKey(Category)
+
 	is_searchable = models.BooleanField(default=True)
 	upload_date = models.DateTimeField(auto_now_add=True)
+
 	number_of_ratings = models.IntegerField(default=0)
 	sum_of_ratings = models.IntegerField(default=0)
+
 	# many branching protocols to one parent protocol
-	last_revision = models.ForeignKey('self', blank=True, null=True)
-	# many different protocols can use one reagent, one protocol can use many reagents
-	reagents = models.ManyToManyField(Reagent)
+	last_revision = models.ForeignKey('self',
+										blank=True,
+										null=True)
+
 	# still need to add steps, which could possible be a separate class instead of just text
 	def __str__(self):
-		return self.title;
+		return self.title
+	def all_reagents(self):
+		return ReagentForProtocol.objects.filter(associated_protocol = self)
+	def all_steps(self):
+		return BasicStep.objects.filter(protocol = self)
+
+# many ReagentForProtocols for each protocol
+class ReagentForProtocol(models.Model):
+	amount = models.DecimalField(max_digits=100, decimal_places=5)
+
+	# (L, mL, g, kg, M), might be better if we had a list of these stored
+	unit = models.CharField(max_length = 10)
+
+	# link it and other of the same type to the right protocol
+	associated_protocol = models.ForeignKey(Protocol)
+
+	reagent = models.ForeignKey(Reagent)
+
+	def __str__(self):
+		return str(self.amount) + self.unit + " " + str(self.reagent)
+
+class ProtocolStep(models.Model):
+	# required fields
+	action = models.TextField()
+
+	# optional fields
+	time = models.IntegerField(blank = True, null = True)
+
+	step_number = models.IntegerField()
+	protocol = models.ForeignKey(Protocol)
+
+	# link the step to the correct reagents
+	associated_reagents = models.ManyToManyField(ReagentForProtocol)
+
+	def __str__(self):
+		result = "(" + str(self.protocol) + ") Step " + str(self.step_number)
+		if self.time:
+			result = result + " for " + str(self.time) + " seconds"
+		return result
