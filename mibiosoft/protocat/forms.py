c67f4d231c239657ba6c2fb248090bbbe7c700e4
Daniel Dineen <dwdineen@umich.edu>
2017-05-04 15:24:05 -0400

    Added inbox

--- mibiosoft/protocat/forms.py
+++ mibiosoft/protocat/forms.py
@@ -3,9 +3,18 @@ from django import forms
 # from django.contrib.auth import authenticate
 
 class NewMessageForm(forms.Form):
-	recipient = forms.CharField(required=False)
-	message = forms.CharField(widget=forms.Textarea)
+	recipient = forms.CharField(required=False, label="To")
+	message = forms.CharField(widget=forms.Textarea, required=False)
 
 	def clean(self, *args, **kwargs):
-		# print(self.cleaned_data)
+		recip = self.cleaned_data.get('recipient')
+		print('here')
+		try:
+			 User.objects.get(username=recip)
+		except:
+			raise forms.ValidationError("User does not exist")
+
+		if self.cleaned_data.get('message').strip() == "":
+			raise forms.ValidationError("Message cannot be empty")
+
 		return super(NewMessageForm, self).clean(*args, **kwargs)
