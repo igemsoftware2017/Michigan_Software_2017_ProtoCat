f2ffb1ec949676a472cc7819478e25acde85cbba
Thomas Oliver <tommy.oliver@live.com>
2017-10-31 14:47:45 -0400

    More

--- mibiosoft/protocat/templatetags/app_filters.py
+++ mibiosoft/protocat/templatetags/app_filters.py
@@ -5,4 +5,4 @@ register = template.Library()
 @register.filter(name='replace_linebr')
 def replace_linebr(value):
     """Replaces all values of line break from the given string with a line space."""
-    return value.replace("<br />", ' ')
+    return value.replace("\n", ' ')
